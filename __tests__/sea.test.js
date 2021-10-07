d3a2ef99eda118945a7321dae90031fc5ec7537e
Dylan-Greathouse <Greathouse.dylanj@gmail.com>
2021-10-06 20:46:17 -0700

    tests for seas table is passing. all 25 crud routes passing!

---
+++ __tests__/sea.test.js
@@ -0,0 +1,88 @@
+import pool from '../lib/utils/pool.js';
+import setup from '../data/setup.js';
+import request from 'supertest';
+import app from '../lib/app.js';
+
+describe('demo routes', () => {
+  beforeAll(() => {
+    return setup(pool);
+  });
+
+  it('post seas to table', () => {
+    return request(app)
+      .post('/api/v1/sea')
+      .send({
+        name: 'seaweed',
+        resource: 'sea',
+      })
+      .then((res) => {
+        expect(res.body).toEqual({
+          id: expect.any(String),
+          sea: expect.any(String),
+          price: expect.any(Number),
+          museum: expect.any(String),
+        });
+      });
+  });
+
+  it('get seas from table seas', () => {
+    request(app).post('/api/v1/sea').send();
+    return request(app)
+      .get('/api/v1/sea')
+      .then((res) => {
+        expect(res.body).toEqual(expect.any(Array));
+      });
+  });
+
+  it('get a sea from table seas', () => {
+    request(app).post('/api/v1/sea').send({
+      name: 'seaweed',
+      resource: 'sea',
+    });
+    return request(app)
+      .get('/api/v1/sea')
+      .then((res) => {
+        expect(res.body).toEqual([
+          {
+            id: expect.any(String),
+            sea: expect.any(String),
+            price: expect.any(Number),
+            museum: expect.any(String),
+          },
+        ]);
+      });
+  });
+
+  it('update a sea from table seas', () => {
+    request(app).post('/api/v1/sea').send({
+      name: 'seaweed',
+      resource: 'sea',
+    });
+    return request(app)
+      .patch('/api/v1/sea/1')
+      .send({
+        id: '1',
+        sea: 'seaweed',
+        price: 1400,
+        museum:
+          'Let it be known that seaweed is a misnomer of the highest order! That is, it is not a noxious weed so much as it is a marine algae most beneficial to life on land and sea. Seaweed, you see, provides essential habitat and food for all manner of marine creatures. And it creates a great deal of the oxygen we land lovers love to breath too, hoo! And yet, I can\'t help but shudder when the slimy stuff touches my toes during a swim. Hoot! The horror!',
+      })
+      .then((res) => {
+        expect(res.body).toEqual({
+          id: expect.any(String),
+          sea: expect.any(String),
+          price: expect.any(Number),
+          museum: expect.any(String),
+        });
+      });
+  });
+
+  it('delete a sea from table seas', async () => {
+    const res = await request(app).delete('/api/v1/sea/1');
+    expect(res.body).toEqual({});
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+});
